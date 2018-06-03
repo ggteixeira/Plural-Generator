2401cbddc2cecd94688e2249a4b467c098311491
guiemi <guiemi.teixeira@gmail.com>
2018-06-03 19:53:33 -0300

    Removendo arquivos locais deletados

--- lauda.py
+++
@@ -1,7 +0,0 @@
-num_char = int(input("Caracteres (com espaço): \n"))
-tam_lauda = 1700 # Uma evolução do programa é criar uma caixa pro usuário poder ajustar o tamanho da lauda
-val_lauda = 4 # Uma outra evolução do programa é criar uma caixa pro usuário poder ajudstar o valor cobrado por lauda
-
-price = ((num_char / tam_lauda) * val_lauda)
-
-print("Preço estipulado: R$ %2.2f" %(price))
