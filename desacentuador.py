6b5ca4f13b153aba83274cf6ac1060223312c784
guiemi <guiemi.teixeira@gmail.com>
2018-06-04 23:18:03 -0300

    Inserindo desacentuador e o plural-generator

---
+++ desacentuador.py
@@ -0,0 +1,9 @@
+# Aqui será construída uma função
+# que auxiliará na remoção de acentos
+# para que palavras como "inglês"
+# sejam geradas como "ingleses" em vez de "inglêses"
+
+acentuadas = ["á", "â", "é", "ê", "í", "ó", "ô", "ú"]
+vogais = ["a", "a", "e", "e", "i", "o", "o", "u"]
+
+
