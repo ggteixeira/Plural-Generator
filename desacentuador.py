f0ca25823bd0b06fd9fae25f3f0e3cfc94978792
guiemi <guiemi.teixeira@gmail.com>
2018-06-11 15:15:34 -0300

    Desacentuador funcionando!

--- desacentuador.py
+++ desacentuador.py
@@ -5,3 +5,23 @@
 
 acentuadas = ["á", "â", "é", "ê", "í", "ó", "ô", "ú"]
 vogais = ["a", "a", "e", "e", "i", "o", "o", "u"]
+
+def desacento(palavra):
+    for letra in palavra:
+        if letra in acentuadas:
+            acentuada = letra
+            index_desacento = acentuadas.index(letra)
+            index_acento = palavra.index(letra)
+            palavra = palavra.replace(acentuada, vogais[index_desacento])
+    return print(palavra)
+desacento("inglêses")
+
+'''
+acentos = {
+    "a": ["á", "â"],
+    "e": ["é", "ê"],
+    "i": ["í"],
+    "o": ["ó", "ô"],
+    "u": ["ú"]
+}
+'''
