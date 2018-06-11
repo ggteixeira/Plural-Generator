06f83a8622eaf6619db6b5a4324fa943c3321fd5
guiemi <guiemi.teixeira@gmail.com>
2018-06-10 21:42:12 -0300

    inserindo comentário no arquivo

---
+++ dicionario-beta.py
@@ -0,0 +1,14 @@
+plurais = {
+    # A:
+    "a": ["s"],
+    # S:
+    "s": ["s", "es"]
+    }
+
+print(plurais["s"][0])
+print(plurais["s"][1])
+
+# Dicionários são melhores que listas neste programa!
+
+# Nota: este dicionário beta precisa ir para um branch de itens que estão em
+# beta
