e809086edabaf291013664e1545b3b532064b135
guiemi <guiemi.teixeira@gmail.com>
2018-06-11 15:32:50 -0300

    Desacentuador corrigido pra rodar como import

--- desacentuador.py
+++ desacentuador.py
@@ -3,14 +3,16 @@
 # para que palavras como "inglês"
 # sejam geradas como "ingleses" em vez de "inglêses"
 
-acentuadas = ["á", "â", "é", "ê", "í", "ó", "ô", "ú"]
-vogais = ["a", "a", "e", "e", "i", "o", "o", "u"]
 
 def desacento(palavra):
+    
+    acentuadas = ["á", "â", "é", "ê", "í", "ó", "ô", "ú"]
+    vogais = ["a", "a", "e", "e", "i", "o", "o", "u"]
+
     for letra in palavra:
         if letra in acentuadas:
             acentuada = letra
             index_desacento = acentuadas.index(letra)
             index_acento = palavra.index(letra)
             palavra = palavra.replace(acentuada, vogais[index_desacento])
-    return print(palavra)
+    return palavra
