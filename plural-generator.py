ce3383b5cc4d734c55d55cf16eeb7e2bf7baa48b
guiemi <guiemi.teixeira@gmail.com>
2018-06-11 15:33:44 -0300

    Oxítonas acentuadas terminadas em "s": funcionando

--- plural-generator.py
+++ plural-generator.py
@@ -1,3 +1,4 @@
+from desacentuador import desacento
 palavra = str(input("Digite: \n"))
 
 # Letras que formam substantivos plurais em PB:
@@ -23,11 +24,12 @@ def plural(palavra):
         if palavra[-2] == "í":
             print(f"{palavra}{s[0]}")
         elif palavra[-2] == "ê":
+            palavra = desacento(palavra)
             print(f"{palavra}{s[0]}")
         else:
             print(f"{palavra}")
 
 plural(palavra)
-
+desacento(palavra)
 acentuadas = ["á", "â", "é", "ê", "í", "ó", "ô", "ú"]
 vogais = ["a", "a", "e", "e", "i", "o", "o", "u"]
