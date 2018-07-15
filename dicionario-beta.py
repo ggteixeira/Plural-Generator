ec074721ee9629f665cb157ec81638bffd7fffdb
guiemi <guiemi.teixeira@gmail.com>
2018-07-14 22:50:42 -0300

    Removendo comentários de teste de branch

--- dicionario-beta.py
+++ dicionario-beta.py
@@ -10,7 +10,3 @@ print(plurais["s"][1])
 
 # Dicionários são melhores que listas neste programa!
 
-# Nota: este dicionário beta precisa ir para um branch de itens que estão em
-# beta
-
-# Isto é uma modificação feita no branch master
